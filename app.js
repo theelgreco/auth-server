001b3f212cda5e5436a95dbc5d50f0a135f9651d
theelgreco <stelios.kakoliris@yahoo.com>
2024-11-24 20:17:58 +0000

    added 0.0.0.0

--- app.js
+++ app.js
@@ -1,38 +1,38 @@
-require('dotenv').config()
-require("./db/connect")
-require("./db/initialise")()
+require("dotenv").config();
+require("./db/connect");
+require("./db/initialise")();
 
 // module imports
-const express = require('express');
-const logger = require('morgan');
-const cors = require('cors')
-const {postSignUp, postLogin} = require("./controllers/controllers");
-const {handleCustomErrors, handlePostgresErrors, handle500Errors} = require("./errors/middleware");
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+const { postSignUp, postLogin } = require("./controllers/controllers");
+const { handleCustomErrors, handlePostgresErrors, handle500Errors } = require("./errors/middleware");
 
 // setup
 const app = express();
-const PORT = process.env.PORT || 9091
+const PORT = process.env.PORT || 9091;
 
 // middleware
-app.use(cors())
-app.use(logger('dev'));
+app.use(cors());
+app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 // endpoints
-app.post("/sign-up", postSignUp)
+app.post("/sign-up", postSignUp);
 
-app.post("/login", postLogin)
+app.post("/login", postLogin);
 
 // error-handling middleware
-app.use(handleCustomErrors)
-app.use(handlePostgresErrors)
-app.use(handle500Errors)
+app.use(handleCustomErrors);
+app.use(handlePostgresErrors);
+app.use(handle500Errors);
 
 // Listener
-app.listen(PORT, () => {
-    console.log("-------------------------------------")
-    console.log(`Server listening on port ${PORT}`)
-    console.log(`View here: http://localhost:${PORT}`)
-    console.log("-------------------------------------")
-})
+app.listen(PORT, "0.0.0.0", () => {
+    console.log("-------------------------------------");
+    console.log(`Server listening on port ${PORT}`);
+    console.log(`View here: http://localhost:${PORT}`);
+    console.log("-------------------------------------");
+});
