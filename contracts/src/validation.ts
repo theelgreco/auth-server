3fab01a758e6270562a5f8de6ddc2b01769c04fe
theelgreco <stelios.kakoliris@yahoo.com>
2025-09-22 20:43:20 +0100

    adds service name enum

--- contracts/src/validation.ts
+++ contracts/src/validation.ts
@@ -1,9 +1,10 @@
 import z from "zod";
+import { ServiceName } from "../../src/generated/prisma/enums.ts";
 
 export const LoginPostData = z.object({
     emailOrUsername: z.string().or(z.email()),
     password: z.string(),
-    serviceName: z.string(),
+    serviceName: z.enum(ServiceName),
 });
 
 export const LoginResponse = z.object({
@@ -14,7 +15,7 @@ export const SignUpPostData = z.object({
     email: z.email(),
     username: z.string(),
     password: z.string(),
-    serviceName: z.string(),
+    serviceName: z.enum(ServiceName),
 });
 
 export const SignUpResponse = z.object({
