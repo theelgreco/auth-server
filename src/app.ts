7658f3781be882c40528099de2af16c81e704062
theelgreco <stelios.kakoliris@yahoo.com>
2025-09-23 15:17:59 +0100

    adds google sign in

--- src/app.ts
+++ src/app.ts
@@ -5,10 +5,11 @@ import cors from "cors";
 import { handleCustomErrors, handlePostgresErrors, handle500Errors } from "./lib/middleware.ts";
 import { createExpressEndpoints, initServer } from "@ts-rest/express";
 import { authContract } from "../contracts/src/contract.ts";
-import { LoginPostData, SignUpPostData } from "../contracts/src/validation.ts";
+import { GoogleSignInData, LoginPostData, SignUpPostData } from "../contracts/src/validation.ts";
 import { createNewUser, getUser } from "./models/models.ts";
 import { checkPassword, hashPassword } from "./lib/utils.ts";
 import { generateJWT } from "./lib/jwt.ts";
+import { User } from "./generated/prisma/client.ts";
 
 dotenv.config();
 
@@ -43,6 +44,30 @@ const authRouter = server.router(authContract, {
         return {
             status: 200,
             body: { msg: "OK" },
+        };
+    },
+    postGoogleSignIn: async ({ body }) => {
+        const { token, serviceName } = GoogleSignInData.parse(body);
+
+        const googleResponse = await fetch("https://www.googleapis.com/oauth2/v3/userinfo", {
+            headers: { Authorization: `Bearer ${token}` },
+        });
+
+        const { email, name, picture } = await googleResponse.json();
+
+        let user: User;
+
+        try {
+            user = await getUser({ emailOrUsername: email, serviceName });
+        } catch {
+            user = await createNewUser({ email, username: name, image: picture, password: "", serviceName });
+        }
+
+        const jwt = generateJWT({ user, serviceName });
+
+        return {
+            status: 200,
+            body: { jwt },
         };
     },
 });
