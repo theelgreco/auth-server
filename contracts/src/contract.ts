258896036ebcaff6f869fab9140395f442c8eb24
theelgreco <stelios.kakoliris@yahoo.com>
2025-09-24 14:25:50 +0100

    adds common error responses to contract

--- contracts/src/contract.ts
+++ contracts/src/contract.ts
@@ -1,34 +1,43 @@
-import { initContract } from "@ts-rest/core";
 import { GoogleSignInData, LoginPostData, LoginResponse, SignUpPostData, SignUpResponse } from "./validation.ts";
+import { contract } from "./init.ts";
+import { commonErrorType } from "./errors.ts";
 
-const contract = initContract();
-
-export const authContract = contract.router({
-    postLogin: {
-        method: "POST",
-        path: "/login",
-        summary: "Login",
-        body: LoginPostData,
-        responses: {
-            200: LoginResponse,
+export const authContract = contract.router(
+    {
+        postLogin: {
+            method: "POST",
+            path: "/login",
+            summary: "Login",
+            body: LoginPostData,
+            responses: {
+                200: LoginResponse,
+            },
+        },
+        postSignUp: {
+            method: "POST",
+            path: "/sign-up",
+            summary: "Sign up",
+            body: SignUpPostData,
+            responses: {
+                200: SignUpResponse,
+            },
+        },
+        postGoogleSignIn: {
+            method: "POST",
+            path: "/google-sign-in",
+            summary: "Sign in with Google",
+            body: GoogleSignInData,
+            responses: {
+                200: LoginResponse,
+            },
         },
     },
-    postSignUp: {
-        method: "POST",
-        path: "/sign-up",
-        summary: "Sign up",
-        body: SignUpPostData,
-        responses: {
-            200: SignUpResponse,
+    {
+        commonResponses: {
+            400: commonErrorType,
+            401: commonErrorType,
+            403: commonErrorType,
+            500: commonErrorType,
         },
-    },
-    postGoogleSignIn: {
-        method: "POST",
-        path: "/google-sign-in",
-        summary: "Sign in with Google",
-        body: GoogleSignInData,
-        responses: {
-            200: LoginResponse,
-        },
-    },
-});
+    }
+);
